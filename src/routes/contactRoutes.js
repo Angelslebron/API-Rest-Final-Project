3970cc28d88bd3f058bafedccdb9e1c4812eac99
htejada762 <hansel2709@gmail.com>
2026-08-19 13:58:35 -0400

    feat: Updated contactRoutes.js

--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -1,26 +1,23 @@
-const express = require('express');const router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 const {
-
   getContacts,
-
   getContactById,
-
   createContact,
-
   updateContact,
-
   deleteContact,
-
 } = require('../controllers/contactController');
 
-
+// Rutas para /api/contacts
 router.route('/')
+  .get(getContacts)
+  .post(createContact);
 
-  .get(getContacts)      // GET    /api/contacts?search=texto  .post(createContact);  // POST   /api/contacts
-
+// Rutas para /api/contacts/:id
 router.route('/:id')
-
-  .get(getContactById)   // GET    /api/contacts/:id  .put(updateContact)    // PUT    /api/contacts/:id  .delete(deleteContact); // DELETE /api/contacts/:id
+  .get(getContactById)
+  .put(updateContact)
+  .delete(deleteContact);
 
 module.exports = router;
